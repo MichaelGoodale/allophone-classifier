c32a6502e185a9d3fbabcc45ac7f8e9b1d0bd584
Michael Goodale <michaelericgoodale@gmail.com>
2019-02-05 19:47:13 -0500

    SOTC update

--- data_classify.py
+++ data_classify.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 import itertools
 import pandas as pd
 import numpy as np
@@ -7,8 +8,9 @@ import graphviz
 import settings as s
 from sklearn import svm, tree
 from sklearn.utils import class_weight, shuffle
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import GridSearchCV
 import keras
 from keras import layers
 import matplotlib.pyplot as plt
@@ -17,20 +19,14 @@ MINIMUM_CONF = -1000000.0
 OVERSAMPLING = True
 TEST_SPLIT = 0.8
 
-def recall(y_true, y_pred):
-    true_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_true * y_pred, 0, 1)))
-    possible_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_true, 0, 1)))
-    return true_positives / (possible_positives + keras.backend.epsilon())
-
-def precision(y_true, y_pred):
-    true_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = keras.backend.sum(keras.backend.round(keras.backend.clip(y_pred, 0, 1)))
-    return true_positives / (predicted_positives + keras.backend.epsilon())
-
-def f1(y_true, y_pred):
-    p = precision(y_true, y_pred)
-    r = recall(y_true, y_pred)
-    return 2*((p*r)/(p+r+keras.backend.epsilon()))
+def plot_history(history):
+    plt.plot(history.history['categorical_accuracy'])
+    plt.plot(history.history['val_categorical_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
 
 
 def get_length(path):
@@ -67,6 +63,7 @@ def evaluate_model(truth, predictions, classes):
 
 prediction_path = os.path.join(s.OUTPUT_DIR, "real_pred")
 feature_slice = [f"feat-{x}" for x in range(63*20)]
+z_scores = {}
 if not os.path.isfile(os.path.join(s.OUTPUT_DIR, "dataframe.pkl")):
     data = pd.read_csv(prediction_path, sep=" ", names=["phoneme", "allophone", "word_pos", "label", "id", "conf", "begin", "end"])
     data["path"] = data['phoneme'].astype(str)+"-"+data['allophone']+"-"+data['word_pos']+"-"+data['label']+"-"+data['id' ].astype(str)
@@ -75,50 +72,111 @@ if not os.path.isfile(os.path.join(s.OUTPUT_DIR, "dataframe.pkl")):
     temp = list(zip(*data["path"].map(get_values)))
     for i, c in enumerate(feature_slice):
         data[c] = temp[i]
+        z_scores[c] = (data[c].mean(), data[c].std(ddof=0))
         data[c] =(data[c] - data[c].mean())/data[c].std(ddof=0)
     data["length"] = data["path"].apply(get_length)
     data.to_pickle(os.path.join(s.OUTPUT_DIR, "dataframe.pkl"))
+    with open(os.path.join(s.OUTPUT_DIR, "z_scores.pkl"), 'wb') as f:
+        pickle.dump(z_scores, f)
 else:
     data = pd.read_pickle(os.path.join(s.OUTPUT_DIR, "dataframe.pkl"))
+    with open(os.path.join(s.OUTPUT_DIR, "z_scores.pkl"), 'rb') as f:
+        z_scores = pickle.load(f)
+
+sotc_prediction_path = os.path.join(s.OUTPUT_DIR, "real_pred_sotc")
+if not os.path.isfile(os.path.join(s.OUTPUT_DIR, "sotc_dataframe.pkl")):
+    sotc_data = pd.read_csv(sotc_prediction_path, sep=" ", names=["phoneme", "measurable", "id", "conf", "begin", "end"])
+    sotc_data["path"] = sotc_data['phoneme'].astype(str)+"-"+sotc_data['measurable'].astype(str)+"-"+sotc_data['id'].astype(str)
+    sotc_data["VOT"] = sotc_data["end"] - sotc_data["begin"]
+    temp = list(zip(*sotc_data["path"].map(get_values)))
+    for i, c in enumerate(feature_slice):
+        sotc_data[c] = temp[i]
+        sotc_datac[c] = (sotc_data[c] - z_scores[c][0])/z_scores[c][1]
+    sotc_data["length"] = sotc_data["path"].apply(get_length)
+    sotc_data.to_pickle(os.path.join(s.OUTPUT_DIR, "sotc_dataframe.pkl"))
+else:
+    sotc_data = pd.read_pickle(os.path.join(s.OUTPUT_DIR, "sotc_dataframe.pkl"))
+
+
+
+for col in ["conf", "VOT", "length"]:
+    data.loc[:, col] = (data[col] - data[col].mean())/data[col].std(ddof=0)
+    sotc_data.loc[:, col] = (sotc_data[col] - data[col].mean())/data[col].std(ddof=0)
+
+#data.loc[:, "allophone"] = data["allophone"].replace([f"{stop}rl" for stop in s.STOPS]+s.STOPS, "yes")
+#data.loc[:, "allophone"] = data["allophone"].replace(["dx", "q"]+[f"{stop}cl" for stop in s.STOPS], "no")
+data.loc[:, "allophone"] = data["allophone"].replace([f"{stop}rl" for stop in s.STOPS]+s.STOPS+["dx", "q"]+[f"{stop}cl" for stop in s.STOPS], "yes")
 
 for stop in s.STOPS:
     stop_data = data[(data["phoneme"] == stop)]
+    #stop_data = data[(data["allophone"] == "yes") | (data["allophone"] == "no")]
     o_data = data[(data["phoneme"] == "vowel") | (data["phoneme"] == "nasal") | (data["phoneme"] == "fric")]
-    o_data.loc[(o_data["phoneme"] == "vowel") | (o_data["phoneme"] == "nasal") | (o_data["phoneme"] == "fric"), "allophone"] = "x"
+    o_data.loc[:, "allophone"] = "no"
     stop_data = stop_data.append(o_data[np.random.rand(len(o_data)) < len(stop_data)/2.5/len(o_data)])
     mask = np.random.rand(len(stop_data)) < 0.8
+
     y, classes = pd.factorize(stop_data.loc[:, "allophone"])
-    #X = stop_data[["conf", "VOT", "length"]+feature_slice]
     X = stop_data[feature_slice]
     X = X.values.reshape((-1, 20, 63))
+    X = np.append(X, np.repeat(stop_data[["conf", "VOT", "length"]].values.reshape(-1, 1, 3), 20, axis=1), axis=2)
 
     train_y = y[mask]
     test_y = y[~mask]
     train_X = X[mask]
     test_X = X[~mask]
-    print(train_X)
 
     if stop == "t":
         n_data = data[(data["phoneme"] == "t+")]
         n_y = n_data.loc[:, "allophone"].apply(lambda x: np.where(classes==x)[0][0])
-        #n_X = n_data[["conf", "VOT", "length"]+feature_slice]
         n_X = n_data[feature_slice]
         n_X = n_X.values.reshape((-1, 20, 63))
+        n_X = np.append(n_X, np.repeat(n_data[["conf", "VOT", "length"]].values.reshape(-1, 1, 3), 20, axis=1), axis=2)
         train_y = np.concatenate((train_y, n_y))
         train_X = np.vstack((train_X, n_X))
 
     train_X, train_y = shuffle(train_X, train_y, random_state=0)
-    unique, counts = np.unique(train_y, return_counts=True)
-    train_y = keras.utils.to_categorical(train_y)
-    model = keras.Sequential()
-    model.add(layers.Conv1D(64, 3, input_shape=(20, 63), activation="relu"))
-    model.add(layers.Conv1D(64, 3, activation="relu"))
-    model.add(layers.GlobalAveragePooling1D())
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(len(classes), activation="softmax"))
-    adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=True)
-    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=["categorical_accuracy",f1, precision, recall])
-    model.fit(train_X, train_y, epochs=25, batch_size=32, validation_split=0.025)
-    pred=np.argmax(model.predict(test_X), axis=1)
 
-    evaluate_model(test_y, pred, classes)
+    #Resample
+    if len(classes) == 2:
+        uniques, counts = np.unique(train_y, return_counts=True)
+        print(counts)
+
+    if len(classes) != 2:
+        train_y = keras.utils.to_categorical(train_y)
+    model = keras.Sequential()
+    model.add(layers.Conv1D(64, 3, input_shape=(20, 66), activation="relu"))
+    model.add(layers.Conv1D(64, 3, activation="relu"))
+    model.add(layers.MaxPooling1D(3))
+    model.add(layers.Conv1D(128, 3, activation="relu"))
+    model.add(layers.GlobalAveragePooling1D())
+    model.add(layers.Dropout(rate=0.5))
+    adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=True)
+
+    if len(classes) == 2:
+        model.add(layers.Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer=adam, metrics=["accuracy"])
+    else:
+        model.add(layers.Dense(len(classes), activation="softmax"))
+        model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=["categorical_accuracy"])
+
+    history = model.fit(train_X, train_y, epochs=10, batch_size=64, validation_split=0.05)
+    if len(classes) == 2:
+        pred = model.predict(test_X)
+    else:
+        pred = np.argmax(model.predict(test_X), axis=1)
+
+    sotc_stop_data = sotc_data[sotc_data["phoneme"] == stop]
+    sotc_X = sotc_stop_data[feature_slice]
+    sotc_X = sotc_X.values.reshape((-1, 20, 63))
+    sotc_X = np.append(sotc_X, np.repeat(sotc_stop_data[["conf", "VOT", "length"]].values.reshape(-1, 1, 3), 20, axis=1), axis=2)
+    sotc_y = sotc_stop_data.loc[:, "measurable"].apply(lambda x: np.where(classes==x)[0][0])
+
+    pred = model.predict(sotc_X)
+    for x in [0.25, 0.5, 0.75, 0.8, 0.85, 0.9, 0.95]:
+        print(x)
+        pred_x = np.copy(pred)
+        pred_x[pred_x > x] = 1
+        pred_x[pred_x <= x] = 0
+        print(confusion_matrix(sotc_y, pred_x))
+    #plot_history(history)
+    #evaluate_model(test_y, pred, classes)
